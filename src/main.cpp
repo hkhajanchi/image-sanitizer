e5f23e5de5e95ad82935519fa3b8238e6f883e6b
Hussain Khajanchi <khajanh1@tcnj.edu>
2020-09-07 23:00:40 -0400

    added some threading tests in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,62 @@
 #include <iostream> 
 #include <opencv2/opencv.hpp> 
+#include <opencv2/core/utility.hpp> 
 #include <thread>
+#include "time.h" 
 
 using namespace cv; 
 
-void printImageShape (Mat image) {
+void imageProcessLambda(Mat image) { 
 
-    if ( !image.data) 
+    std::cout << image.size().height << '\n'; 
+    std::cout << image.size().width  << '\n'; 
+    std::cout << "Image Processing completed" << '\n';
+} 
+
+void runThreaded (std::vector<Mat> images) {
+    std::vector<std::thread> threadPool; 
+    for (size_t i = 0; i < images.size(); i++)
     {
-        printf("No Image data \n" ); 
+        threadPool.push_back(std::thread(imageProcessLambda, images[i]));
     }
-
-    else 
-    {
-        printf("%i \n", image.size().height);
-        printf("%i \n", image.size().width);
+    for (auto& thread: threadPool) {
+        thread.join(); 
     }
-
 }
 
-void printTestMessage () {
-    std::cout << "In a thread" << "\n";
+void runNonThreaded(std::vector<Mat> images) {
+    for (size_t i = 0; i < images.size(); i++)
+    {
+        imageProcessLambda(images[i]);
+    }
 }
 
+void processPaths (std::vector<cv::String> paths, int i) {
+    std::vector<Mat> image_batch; 
+
+      for (int j = i; j < i + 3200; j++) {
+          image_batch.push_back(imread(paths[j])); 
+      }
+}
 int main(int argc, char** argv)  {
 
-    if (argc != 2)
-    {
-        printf( "usage: DisplayImage.out <Image_Path>\n"); 
-        return -1; 
-    }
+    // Load all images in GrassClover dataset 
+    std::vector<cv::String> paths; 
+    glob("/images/all_images/*.jpg", paths, false); 
+    std::cout << paths.size() << "\n"; 
 
-    Mat image; 
-    image = imread(argv[1], 1); 
+    auto cnt = paths.size(); 
+    int data_chunk = 3200; // load in 3200 images at any given iteration 
+  /* 
+  We have 32000 images, which obviously can't be loaded all into RAM without crashing the computer 
+  Therefore, i'm loading 3200 images at a time, and dividing it to 32 threads 
+  */
 
-    std::thread test(printImageShape,image);
-    std::thread test2(printTestMessage);
+  for (int i = 0; i < cnt; i+=data_chunk)   
+  {
+      processPaths(paths, i);
+      std::cout << "Chunk " << i << "Processed." << "\n";
 
-    test.join(); 
-    test2.join(); 
-
-    std::cout << "Threads completed" <<'\n'; 
-
-  
-    
+  } 
 
 }
